38049df57d4b109932bda962b67e48a9698d3f0a
samueldamasceno <samueldamasceno.vip@gmail.com>
2024-08-16 08:53:34 -0300

    estrutura base da classe livro

--- src/models/Livro.java
+++ src/models/Livro.java
@@ -1,4 +1,92 @@
 package models;
 
+import enums.Categoria;
+import enums.Genero;
+
+import java.time.LocalDate;
+
 public class Livro {
+    private String id;
+    private String titulo;
+    private String autor;
+    private LocalDate dataPublicacao;
+    private Categoria categoria;
+    private Genero genero;
+    private String editora;
+    private int numeroPaginas;
+
+    public Livro(String id, String titulo, String autor, LocalDate dataPublicacao, Categoria categoria, Genero genero, String editora, int numeroPaginas) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataPublicacao = dataPublicacao;
+        this.categoria = categoria;
+        this.genero = genero;
+        this.editora = editora;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
 }
